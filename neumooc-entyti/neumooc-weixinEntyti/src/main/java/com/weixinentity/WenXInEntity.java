0fdd90f94a0251c848f60d9a0b8fa0e41132a3d9
123 <123@DESKTOP-ED4H62K>
2020-11-17 16:18:02 +0800

    Signed-off-by: 123 <123@DESKTOP-ED4H62K> Signed-off-by: root <307372101@qq.com>

---
+++ neumooc-entyti/neumooc-weixinEntyti/src/main/java/com/weixinentity/WenXInEntity.java
@@ -0,0 +1,15 @@
+package com.weixinentity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain=true)
+public class WenXInEntity {
+	
+	private int appId;
+	private String appName;
+	
+	
+
+}
